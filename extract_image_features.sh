dd20c94e1a9a3ea6c2b2f2a5d0d20fcff81b360b
Pablo Fonseca <palefo@gmail.com>
2019-02-02 02:06:41 -0500

    Added script to extract features from the images

---
+++ extract_image_features.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+python scripts/extract_features.py \
+  --input_image_dir data/CLEVR_v1.0/images/train \
+  --output_h5_file data/train_features.h5
+
+python scripts/extract_features.py \
+  --input_image_dir data/CLEVR_v1.0/images/val \
+  --output_h5_file data/val_features.h5
+
+python scripts/extract_features.py \
+  --input_image_dir data/CLEVR_v1.0/images/test \
+  --output_h5_file data/test_features.h5
